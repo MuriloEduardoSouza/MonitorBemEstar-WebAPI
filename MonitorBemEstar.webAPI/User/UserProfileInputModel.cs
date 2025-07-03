83e4727685e58eb6603f0a74d07790ef79bd45d7
Murilo Eduardo <muheduardo96@gmail.com>
2025-07-03 20:12:33 -0300

    Corrigir rastreamento de arquivos ignorados

---
+++ MonitorBemEstar.webAPI/User/UserProfileInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonitorBemEstar.webAPI.User;
+
+public class UserProfileInputModel
+{
+    [Required(ErrorMessage="Nome completo é obrigatório.")]
+    public string NomeCompleto { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Idade é obrigatória.")]
+    public int Idade { get; set; }
+
+    [Required(ErrorMessage = "Endereço é obrigatório.")]
+    public string Endereco { get; set; } = string.Empty;
+}
+
